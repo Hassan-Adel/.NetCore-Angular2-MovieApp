ed0ed68abcf9b5ae07fb698e578edd2352213ff7
hassan.adel <hassan.adel@HASSANADEL-PC>
2017-06-05 00:54:25 +0200

    Add project files.

---
+++ AngularJS2Setup/app/app.component.ts
@@ -0,0 +1,10 @@
+﻿import { Component } from "@angular/core";
+
+@Component({
+    selector: "my-app",
+    template: "<h1>{{WelcomeMessage}}</h1>"
+})
+
+export class AppComponent {
+WelcomeMessage: string = "Welcome to my site"
+}
