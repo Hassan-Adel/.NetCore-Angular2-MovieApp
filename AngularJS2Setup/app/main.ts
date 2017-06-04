f2941ab4f87193298f7522d8c40eb56ec5e84caa
Hassan-Adel <hasanadel555@hotmail.com>
2017-06-05 01:13:55 +0200

    Bootstrapping the App

--- AngularJS2Setup/app/main.ts
+++ AngularJS2Setup/app/main.ts
@@ -1,3 +1,6 @@
-﻿
-import { AppModule } from './app.module';
+﻿import { AppModule } from './app.module';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
+const platform = platformBrowserDynamic();
+
+platform.bootstrapModule(AppModule);
