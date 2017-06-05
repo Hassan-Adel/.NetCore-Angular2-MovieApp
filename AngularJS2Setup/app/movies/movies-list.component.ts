2a98c5d7f862a4555be59fe9b3e71308b4c83e26
Hassan-Adel <hasanadel555@hotmail.com>
2017-06-05 04:08:54 +0200

    Add movie list component

---
+++ AngularJS2Setup/app/movies/movies-list.component.ts
@@ -0,0 +1,11 @@
+﻿import { Component } from "@angular/core";
+
+@Component({
+    moduleId: module.id,
+    selector: "my-app",
+    templateUrl: "movies-list.component.html"
+})
+
+export class MoviesListComponent {
+    WelcomeMessage: string = "Welcome to my site"
+}
