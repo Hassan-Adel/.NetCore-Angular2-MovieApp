f4b62610727efcff95f3449709f5e09c336badbc
Hassan-Adel <hasanadel555@hotmail.com>
2017-06-10 04:53:13 +0200

    Using Interpolation

--- AngularJS2Setup/app/movies/movie-list.component.js
+++ AngularJS2Setup/app/movies/movie-list.component.js
@@ -11,7 +11,28 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 var core_1 = require("@angular/core");
 var MoviesListComponent = (function () {
     function MoviesListComponent() {
-        this.WelcomeMessage = "Welcome to my site";
+        this.pageTitle = "Movies List";
+        this.movies = [
+            {
+                "movieId": 2,
+                "movieName": "Titanic!",
+                "movieStar": "DiCaprio",
+                "description": "A seventeen-year-old aristocrat falls in love with a kind but poor artist aboard the luxurious, ill-fated R.M.S. Titanic.",
+                "releaseDate": "3/13/2016",
+                "price": 8.00,
+                "starRating": 4.5,
+                "imageUrl": "http://ia.media-imdb.com/images/M/MV5BMzg1MDA0MTU2Nl5BMl5BanBnXkFtZTcwMTMzMjkxNw@@._V1_.jpg"
+            },
+            {
+                "movieId": 3,
+                "movieName": "Jaws!",
+                "movieStar": "Shaw",
+                "description": "When a gigantic great white shark begins to menace the small island community of Amity, a police chief, a marine scientist and a grizzled fisherman set out to stop it.",
+                "releaseDate": "4/13/2016",
+                "price": 6.00,
+                "starRating": 4.8,
+                "imageUrl": "http://ia.media-imdb.com/images/M/MV5BNDcxODkyMjY4MF5BMl5BanBnXkFtZTgwOTk5NTc5MDE@._V1_SY1000_CR0,0,666,1000_AL_.jpg"
+            }];
     }
     MoviesListComponent = __decorate([
         core_1.Component({
